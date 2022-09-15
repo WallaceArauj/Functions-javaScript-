a9434bd0a53976a9789a4dbe5eeb81c71944f8d4
Wallace Araujo <112869411+WallaceArauj@users.noreply.github.com>
2022-09-15 17:00:04 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,18 @@
+function dados (){
+   
+    const alturaTxt = 0
+    const pesoTxt = 0 
+
+    let altura = Number(alturaTxt)((' Digite sua altura: '));
+    let peso = Number(pesoTxt)(('Digite seu peso: '));
+
+    
+    soma = (altura * altura) / peso
+    
+    return altura;
+    return peso;
+    return soma;
+    
+}
+
+module.exports = dados
---
+++ functionsJS.js
@@ -0,0 +1,16 @@
+const prompt = require ('prompt-sync')();
+
+const dados = require ('./dados')();
+
+peso = 0
+altura = 0
+
+dados();
+
+console.clear();
+
+console.log(`O IMC é: ${soma}`)
+
+
+
+
